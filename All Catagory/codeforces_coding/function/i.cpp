061c1e30cfe1cd9019a7eb5a07a54864e3d7bb7c
unknown <mdjulkarnaen7@gmail.com>
2022-01-19 12:03:30 +0600

    commit-function-i

--- All Catagory/codeforces_coding/function/i.cpp
+++ All Catagory/codeforces_coding/function/i.cpp
@@ -1,1 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void matrix(int n,int x,int y){
+
+    int arr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==x-1)swap(arr[x-1][j],arr[y-1][j]);
+
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(j==x-1)swap(arr[i][x-1],arr[i][y-1]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(j==n-1)cout<<arr[i][j];
+            else cout<<arr[i][j]<<" ";
+        }cout<<endl;
+    }
+
+}
+
+int main(){
+
+    int n,x,y;
+    cin>>n>>x>>y;
+    matrix(n,x,y);
+
+    return 0;
+}
 
