04177d056c1ef417f51314af1d5ff753bb43c26f
unknown <mdjulkarnaen7@gmail.com>
2022-01-19 11:00:11 +0600

    commit-function-h

--- All Catagory/codeforces_coding/function/h.cpp
+++ All Catagory/codeforces_coding/function/h.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void print(int n){
+    int m;
+    char ch;
+    for(int i=0;i<n;i++){
+        cin>>m>>ch;
+        for(int j=1;j<=m;j++){
+            if(j==m){
+                cout<<ch;
+            }else {
+                cout<<ch<<" ";
+            }
+        }cout<<endl;
+    }
+
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    print(n);
+
+    return 0;
+}
