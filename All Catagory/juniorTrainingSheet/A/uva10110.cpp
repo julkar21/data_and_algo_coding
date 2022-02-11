d48e46d48c74525910ad608af6db58f9dfc49383
julkar21 <mdjulkarnaen7@gmail.com>
2022-02-11 14:28:55 +0600

    junior-sheet-uva

---
+++ All Catagory/juniorTrainingSheet/A/uva10110.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    unsigned int n;
+    while(cin>>n && n!=0){
+        unsigned int sq = (int) sqrt(n);
+        cout<<(sq*sq==n?"yes\n":"no\n");
+    }
+
+    return 0;
+}
