4622769c97b77dbcc39f1f35baa1c8229fdb8930
julkar21 <mdjulkarnaen7@gmail.com>
2023-03-30 15:06:30 +0600

    folder name changed

---
+++ cpi12-contest/Math/Codforces-271B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define LIM 100005
+using namespace std;
+int a[500][500];
+
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
+}
--- practice/bfs.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-
-    vector<int>adj[100];
-    int visited[100];
-
-    return 0;
-}
